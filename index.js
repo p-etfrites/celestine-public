0082bbe7aa79f6cb1e17d8e21388880307daf8e1
Timmy Turner <38393475-arsehoefvk@users.noreply.replit.com>
2024-12-16 06:16:13 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => {
+  console.log('Project is running!');
+})
+
+app.get("/" , (req, res) => {
+  res.send("Hello world!");
+})
+
+const Discord = require("discord.js");
+const client = new Discord.Client({intents: ["GUILDS" , "GUILD_MESSAGES"]});
+client.on("message", message => {
+  if(message.content === "ping") {
+    message.channel.send("pong")
+  }
+})
+
+client.login(process.env.token);
