292828c2533dce8cea0492fec00621516e662290
A <arsehoefvk@gmail.com>
2025-01-09 22:39:36 +0700

    Update index.js
    delete secrets

--- index.js
+++ index.js
@@ -46,14 +46,14 @@ for (const file of commandFiles) {
 
 //Deploy commands
 const rest = new REST().setToken(
-  "MTMxNzczNjcwMTE1NzgzODg5MQ.GWP_kQ.x0UNVuAhZ9QcGnZGLFCcmAL2h-gMRgDV1mXzGc",
+  'YOUR_TOKEN',
 );
 
 async function deployCommands() {
   try {
     console.log("Started refreshing application (/) commands.");
 
-    await rest.put(Routes.applicationCommands("1317736701157838891"), {
+    await rest.put(Routes.applicationCommands('YOUR_CLIENT_ID'), {
       body: commands,
     });
 
@@ -89,45 +89,4 @@ client.on("interactionCreate", async (interaction) => {
   }
 });
 
-client.login(process.env.token);
-
-/*const express = require('express');
-const app = express();
-
-app.listen(3000, () => {
-  console.log('Project is running!');
-})
-
-app.get("/" , (req, res) => {
-  res.send("Hello world!");
-})
-const fs = require('node:fs');
-const path = require('node:path');
-const Discord = require("discord.js");
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-const client = new Discord.Client({intents: ["GUILDS" , "GUILD_MESSAGES"]});
-client.on("message", message => {
-  if(message.content === "ping") {
-    message.channel.send("pong")
-  }
-})
-client.login(process.env.token);
-client.commands = new Collection();
-
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
-
-for (const folder of commandFolders) {
-  const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-  for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    // Set a new item in the Collection with the key as the command name and the value as the exported module
-    if ('data' in command && 'execute' in command) {
-      client.commands.set(command.data.name, command);
-    } else {
-      console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-    }
-  }
-}*/
+client.login('YOUR_TOKEN');
